ae8f2813f386276ce73b014d51cac65f8ad22614
LappisProblems <92263830+LappisProblems@users.noreply.github.com>
2023-05-06 16:58:37 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='LappisDistributions',
+    version='0.0.1',
+    author='Timothy Chapman',
+    author_email='timothy.chapman98@outlook.com',
+    url='https://github.com/LappisProblems/LappisDistributions',
+    license='MIT',
+    packages=['LappisDistributions'],
+    description='A collection of selected distributions',
+)
