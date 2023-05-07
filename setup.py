e1a206a5be233f95fe560d446d099ac9a333f8a0
LappisProblems <92263830+LappisProblems@users.noreply.github.com>
2023-05-07 22:43:30 +0200

    Again

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-from distutils import find_namespace_packages
 setup(
     name='LappisDistributions',
     version='0.0.1',
@@ -7,7 +6,7 @@ setup(
     author_email='timothy.chapman98@outlook.com',
     url='https://github.com/LappisProblems/LappisDistributions',
     license='MIT',
-    packages=find_namespace_packages(where='src'),
+    packages='distributions',
     description='A collection of selected distributions',
     install_requires=[]
 )
