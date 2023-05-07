c740f62a4efd987cd5d050489207884a01b78961
LappisProblems <92263830+LappisProblems@users.noreply.github.com>
2023-05-07 22:51:30 +0200

    Again

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
-setup(
-    name='LappisDistributions',
-    version='0.0.1',
-    author='Timothy Chapman',
-    author_email='timothy.chapman98@outlook.com',
-    url='https://github.com/LappisProblems/LappisDistributions',
-    license='MIT',
-    packages=['distributions'],
-    description='A collection of selected distributions',
-    install_requires=[]
+from setuptools import setup, find_packages
+
+setup(name='LappisDistributions',
+      version='0.0.1',
+      author='Timothy Chapman',
+      author_email='timothy.chapman98@outlook.com',
+      package_dir={'':'src'},
+      packages=find_packages('src'),
+      install_requires=[],
+      zip_safe=False,
+      licence='MIT'
 )
